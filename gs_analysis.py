73bd3192a274c97e53925f4fb53e29b24365b62a
py1sl <py1sl@users.noreply.github.com>
2022-01-05 11:29:10 +0000

    [skip ci] Apply formatting changes

--- gs_analysis.py
+++ gs_analysis.py
@@ -262,43 +262,46 @@ def get_spect(path):
 
     return counts, ebins
 
+
 def peak_finder(x, prominence, wlen):
-    """ Identifies the peaks and returns their index """
+    """Identifies the peaks and returns their index"""
 
     sf = five_point_smooth(x)
     smooth = np.array(sf)
     sf2 = five_point_smooth(smooth)
     smooth2 = np.array(sf2)
-    peaks,_ = find_peaks(sf2, prominence = prominence, wlen = wlen)
-    
-    return(smooth2, peaks)
+    peaks, _ = find_peaks(sf2, prominence=prominence, wlen=wlen)
+
+    return (smooth2, peaks)
+
 
 def peak_identifier(smooth_counts, ebins, peaks):
-    """ Plots the smoothed spectra and highlights the peaks on the plot """
-    
-    plt.plot(ebins[peaks], smooth_counts[peaks], "xr"); plt.plot(ebins, smooth_counts)
-    plt.xlabel('ebins')
-    plt.ylabel('counts')
-    plt.yscale('log') 
+    """Plots the smoothed spectra and highlights the peaks on the plot"""
+
+    plt.plot(ebins[peaks], smooth_counts[peaks], "xr")
+    plt.plot(ebins, smooth_counts)
+    plt.xlabel("ebins")
+    plt.ylabel("counts")
+    plt.yscale("log")
     plt.show
-    
+
 
 def peak_counts(peaks, index, smooth_counts, ebins):
-    """ Index is the peak array index for the peak that counts is required for
-        i.e [0], NOT the peak index itself i.e [3210]
-        Returns the index of the peak and its calculated net count
-    """    
+    """Index is the peak array index for the peak that counts is required for
+    i.e [0], NOT the peak index itself i.e [3210]
+    Returns the index of the peak and its calculated net count
+    """
     x, y = get_peak_roi(peaks[index], smooth_counts, ebins, offset=10)
-    
+
     length = len(x)
     start_pos = x[0]
     end_pos = x[length - 1]
-    start, = np.where(ebins == start_pos)
-    end, = np.where(ebins == end_pos)
-    
+    (start,) = np.where(ebins == start_pos)
+    (end,) = np.where(ebins == end_pos)
+
     counts = net_counts(smooth_counts, start[0], end[0], m=1)
-    
-    return(peaks[index], counts)
+
+    return (peaks[index], counts)
 
 
 if __name__ == "__main__":
